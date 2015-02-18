928a2578a841eb95e1eca57f6dcd9be0746528b5
Daniel Johnson <danielj41@gmail.com>
2015-02-18 13:12:07 -0800

    Working on refactoring

---
+++ source/js/Gun.js
@@ -0,0 +1,28 @@
+(function (window) {
+
+    function Gun() {
+        this.Container_constructor();
+        this.addChild(new createjs.Bitmap(preload.getResult("gun")));
+        this.on("tick", p.tick);
+
+        addKeyHandler(this, "key-right", this.test);
+        
+    }
+    var p = createjs.extend(Gun, createjs.Container);
+    window.Gun = createjs.promote(Gun, "Container")
+
+    p.tick = function (event) {
+        this.x += 3;
+        this.y += 3;
+    }
+
+    p.test = function (event) {
+        this.x -= 5;
+        if(this.x > 100) {
+            removeObject(this);
+            console.log("what");
+        }
+    }
+
+
+}(window));
